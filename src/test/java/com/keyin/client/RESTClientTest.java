24b05e0c8785292a88c566decb9ff3298a783710
BL-Keyin <you@example.com>
2024-03-06 10:10:18 -0500

    // Refactor by Implementing Dependency Injection
    - Introduced dependency injection in HTTPRestCLIApplication
    - Added parameterized constructor to RESTClient to support DI
    - Removed direct implementation of RESTClient within HTTPRestCLIApplication
    - Updated RESTClientTest to align with RESTClient's new constructor requirement
    - Streamlined RESTClient's buildAirportListFromResponse method
    - Removed unused instantiations and redundant code in HTTPRestCLIApplication
    - Added new entity ApplicationConfig to handle new configuration setup
    - Added Application.Properties so URI can be swapped with just a single change
    
    Changes to be committed:
    	new file:   src/main/java/com/keyin/config/ApplicationConfig.java
    	modified:   src/main/java/com/keyin/http/cli/HTTPRestCLIApplication.java
    	modified:   src/main/java/com/keyin/http/client/RESTClient.java
    	new file:   src/main/resources/application.properties
    	modified:   src/test/java/com/keyin/HTTPRestCLIApplicationTest.java
    	modified:   src/test/java/com/keyin/client/RESTClientTest.java

--- src/test/java/com/keyin/client/RESTClientTest.java
+++ src/test/java/com/keyin/client/RESTClientTest.java
@@ -1,5 +1,6 @@
 package com.keyin.client;
 
+import com.keyin.config.ApplicationConfig;
 import com.keyin.domain.Airport;
 import com.keyin.http.client.RESTClient;
 import org.junit.jupiter.api.Assertions;
@@ -7,13 +8,19 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+
 public class RESTClientTest {
+    // Test now initializes the config and grabs the URI
+        ApplicationConfig config = new ApplicationConfig();
+        String airportServiceUri = config.getAirportServiceUri();
+    
+
     @Test
     public void testBuildAirportListFromResponse() throws Exception {
         String jsonResponse = "[\n{\n\"id\": 1,\n\"name\": \"St. John's\",\n\"code\": \"YYT\"\n},\n" +
                 "{\n\"id\": 2,\n\"name\": \"Deer Lake\",\n\"code\": \"YDF\"\n}\n]";
 
-        RESTClient restClientUnderTest = new RESTClient();
+        RESTClient restClientUnderTest = new RESTClient(airportServiceUri);
 
         List<Airport> airportList = restClientUnderTest.buildAirportListFromResponse(jsonResponse);
 
