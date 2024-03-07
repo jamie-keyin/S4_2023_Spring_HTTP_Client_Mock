8f9e04afd8db07e18ce527538f5111a4d4761bc5
joshwhite2 <126173462+joshwhite2@users.noreply.github.com>
2024-03-07 19:58:52 -0330

    Replaced hardcoded value with config.json file

--- src/main/java/com/keyin/http/client/RESTClient.java
+++ src/main/java/com/keyin/http/client/RESTClient.java
@@ -3,10 +3,12 @@ package com.keyin.http.client;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.keyin.domain.Airport;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -18,24 +20,35 @@ public class RESTClient {
     public List<Airport> getAllAirports() {
         List<Airport> airports = new ArrayList<Airport>();
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/airports")).build();
+        ObjectMapper objectMapper = new ObjectMapper();
 
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode()==200) {
-                System.out.println("***** " + response.body());
-            } else {
-                System.out.println("Error Status Code: " + response.statusCode());
-            }
-
-            airports = buildAirportListFromResponse(response.body());
-
-
-        } catch (IOException | InterruptedException e) {
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.json")) {
+            JsonNode jsonNode = objectMapper.readTree(input);
+            String baseUrl = jsonNode.get("apiUrl").asText();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(baseUrl + "/airports"))
+                    .build();
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("http://localhost:8080/airports")).build();
+//
+//        try {
+//            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+//            if (response.statusCode()==200) {
+//                System.out.println("***** " + response.body());
+//            } else {
+//                System.out.println("Error Status Code: " + response.statusCode());
+//            }
+//
+//            airports = buildAirportListFromResponse(response.body());
+//
+//
+//        } catch (IOException | InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
 
         return airports;
     }
